860f8b2d035d544481d09a958ca110a52e3865e0
icemberg <abhiraams2004@gmail.com>
2026-09-01 15:58:52 +0530

    feat: Implement the about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import gsap from 'gsap';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Cocktails from '../components/Cocktails';
+import About from '../components/About';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -15,6 +16,7 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   )
 }
